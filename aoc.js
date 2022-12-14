92ad7049badf2b65852fd43fffef23b41951b434
João Paulo <joao.paulo@vouch.io>
2022-12-14 15:26:31 -0300

    Day 14

--- aoc.js
+++ aoc.js
@@ -1,10 +1,12 @@
 // Sort by the time the star was acquired in a certain day.
 // temp1 is a private board JSON API result, like in https://adventofcode.com/2022/leaderboard/private/view/217019.json
 
-let day = 6
-let star = 1
+let day = 14
+let star = 2
+let starTime = timestamp => (new Date(timestamp*1000)).toTimeString().substring(0,8)
 Object.values(temp1.members)
-  .map(i => [i.name, i.id, i.completion_day_level[day]])
-  .filter(i => i[2])
-  .filter(i => i[2][star])
-  .sort((a,b) => a[2][star].get_star_ts - b[2][star].get_star_ts)
+  .map(i => [i.name, i.completion_day_level[day]])
+  .filter(i => i[1])
+  .filter(i => i[1][star])
+  .sort((a,b) => a[1][star].get_star_ts - b[1][star].get_star_ts)
+  .map(([name, stars]) => [starTime(stars[1]?.get_star_ts), starTime(stars[2]?.get_star_ts), name])
